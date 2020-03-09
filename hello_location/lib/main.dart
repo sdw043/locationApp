8a358c14291c96cbeb26fc7873c81dca51cb6e41
Spencer Ward <54869250+sdw043@users.noreply.github.com>
2020-03-09 09:57:52 -0500

    uh

--- hello_location/lib/main.dart
+++ hello_location/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:io';
 
 void main() => runApp(App());
 
@@ -20,9 +21,25 @@ class _AppState extends State<App> {
     setState(() {
       _locationMessage = "${position.latitude}, ${position.longitude}";
     });
-
   }
 
+//  void _connectToPi() async {
+//    int portNo = 12345;
+//    Socket.connect('192.169.1.143', portNo).then((socket) {
+//      if(socket == null){
+//        print("something very wrong happened with your socket, sir!");
+//      }
+//      else{
+//        socket.write('pinging mr. pi!');
+//        print('pinging the pi right now');
+//      }
+//    });
+//  }
+
+  void _connectToPi() async {
+    int portNo = 12345;
+    //RawSocket()
+  }
 
   // This widget is the root of your application.
   @override
@@ -37,13 +54,16 @@ class _AppState extends State<App> {
             child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-            Text(_locationMessage), 
+            Text(_locationMessage, style: TextStyle(fontSize: 30),),
             FlatButton(
               onPressed: () {
+                for(int i = 0; i < 4; i++){
                   _getCurrentLocation();
+                }
+                _connectToPi();
               },
               color: Colors.green,
-              child: Text("Find Location")
+              child: Text("Find Location", style: TextStyle(fontSize: 50),)
             )
           ]),
         )
